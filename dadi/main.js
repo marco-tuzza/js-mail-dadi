fa6a8315373892ceafa5b3d95e22c73d28b32695
Marco Tuzza <marcotuzza@hotmail.it>
2020-04-21 18:54:19 +0200

    Gioco coi dadi bonus

--- dadi/main.js
+++ dadi/main.js
@@ -16,3 +16,25 @@ if (numero_giocatore > 6) {
 else {
     console.log('Peccato! Hai perso!');
 }
+
+
+
+
+
+
+// Consiglio, il gioco qui sopra non è particolarmente divertente quindi ve ne propongo un altro:
+
+var numero_giocatore2 = prompt('Scegli un numero da 1 a 6');
+console.log(numero_giocatore2);
+
+var puntata = prompt('Scegli una cifra da puntare (in monete virtuali) tra 1 e 100')
+
+var numero_cpu2 = (Math.floor(Math.random() * 6) + 1);
+console.log(numero_cpu2);
+
+if (numero_giocatore2 == numero_cpu2) {
+    console.log('Congratulazioni! Hai vinto!');
+    console.log('Ecco la tua vincita: ' + (puntata * 6) + ' monete, comprati un bel meme');
+} else {
+    console.log('Mi spiace! Hai perso!');
+}
